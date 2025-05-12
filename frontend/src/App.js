83d2adc08d3f0678f149cb589612ace6a51b3b1f
Elias Zebene <zebene.elias@hotmail.com>
2025-05-12 18:12:57 +0100

    First API Call to Unsplash.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
-
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-function App() {
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
+function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-    // Handle the search submit event
+    e.preventDefault();
     console.log("Search submitted", searchTerm);
+
+    fetch(`https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${UNSPLASH_KEY}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Search results:", data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }
-  // This function will be called when the form is submitted
-  console.log("searchTerm", searchTerm);
 
   return (
     <div className="App">
